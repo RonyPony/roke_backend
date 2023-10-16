c265a83532f6da9c014e278389fc8f8b3093dd9e
Ronel Cruz <ronel.cruz.a8@gmail.com>
2023-10-15 23:02:54 -0400

    Create months.sql

---
+++ RokeBackend/scripts/months.sql
@@ -0,0 +1,47 @@
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Enero', 0);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Febrero', 1);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Marzo', 2);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Abril', 3);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Mayo', 4);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Junio', 5);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Julio', 6);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Agosto', 7);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Septiembre', 8);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Octubre', 9);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Noviembre', 10);
+
+INSERT INTO Roke.dbo.months
+(id, [Month], [order])
+VALUES(NEWID(), 'Diciembre', 11);
